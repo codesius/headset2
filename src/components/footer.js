fb71e7d99863b4e4d6ebf6caf68e40f64c609bb6
zimwizy <djeghri.mohamed@uab.ro>
2024-04-21 22:29:17 +0300

    Description of changes made

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../styles/footer.css'
+import Codesius from '../assets/img/codesius.svg'
 
 function footer() {
   return (
     <div className='footer'>
         <div className='footer-container'>
+            <img src={Codesius} alt='Codesius' />
             <p>DEV.CODESIUS@GMAIL.COM</p>
             <p className='last'>Copyright © CODESIUS 2024</p>
         </div>
